a54d1f49d36641626e72261f555032e53f705d97
ADINATH AJITH NAIK <naik.adi2001@gmail.com>
2021-07-05 17:41:42 +0530

    Added 2nd API

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port=3000;
 
 
 app.get('/',(request, response)=>{
-    response.status(200).send("<h1>Hello There</h1>")
+    response.status(200).send("<h1>Hello There</h1>");
+})
+
+app.get('/healthCheck',(request, response)=>{
+    response.status(200).send("<h2> Server is UP</h2>");
 })
 
 app.listen(port,()=>{
