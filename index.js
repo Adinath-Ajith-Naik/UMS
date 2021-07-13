3af4d71d2ef0ff534e11520aee051279b52dc5b4
ADINATH AJITH NAIK <naik.adi2001@gmail.com>
2021-07-13 22:35:12 +0530

    Added API for Array

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port=3000;
 
+var arr=['Books','Pencil','Pen','Reference Guide'];
 
 app.get('/',(request, response)=>{
     response.status(200).send("<h1>Welcome to Java Script</h1>");
@@ -12,6 +13,16 @@ app.get('/healthCheck',(request, response)=>{
     response.status(200).send("<h2> Server is UP! Welcome to Health Check </h2>");
 })
 
+app.get('/array',(request,response)=>{
+    response.status(200).send(arr);
+})
+
 app.listen(port,()=>{
     console.log("Server Up! Welcome to  ",port);
-})
+    console.log(arr);
+        
+});
+
+/*const arr  = [
+    {name: 'Books','Pen','Pencil','Referece Materials'};
+]*/
