fdb36a9312b4ed31816b5abf31aaaa6c3e74ec54
ADINATH AJITH NAIK <naik.adi2001@gmail.com>
2021-07-15 20:43:07 +0530

    Changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //const { response } = require("express");
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -47,11 +48,44 @@ app.post('/login', (request, response) => {
     console.log("hello login");
     console.log(request.body.Username);
     var uname=request.body.Username;
-    users.forEach((user) => {
+    var upw=request.body.Password
+    var name = users.forEach((user) => {
         console.log(user.userName)
     });
+
+    var pw = users.forEach((user) => {
+        console.log(user.password)
+    });
+
+   // var name=users.forEach(user.name);
+    //var pw=users.forEach(user.password);'
+
+    console.log("*************************");
+    console.log(uname);
+
+    if(uname== name && upw==pw )
+    {
+        respond.send("Login Successful !!!")
+    }
+    else
+    {
+        respond.send("Login Failed !!! Please check the credentials.. ");
+        respond.send("Please check again");
+    }
     response.send("OK");
     
+})
+
+app.post('/addition',(request,response)=>{
+    console.log("Addition taking place.")
+
+    var v1= request.body.a;
+    var v2= request.body.b;
+
+    var sum =Number(request.body.a)+ Number(request.body.b);
+
+    repsond.send(sum);
+
 })
 
 app.listen(port, () => {
