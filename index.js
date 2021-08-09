e08e31bafad724233ab97518a11d8bfc0000063d
ADINATH AJITH NAIK <naik.adi2001@gmail.com>
2021-08-09 16:27:39 +0530

    Created API for New User Creation

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 //const { response } = require("express");
-const {
-    response
-} = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -46,11 +43,14 @@ app.get('/users', (request, response) => {
     response.status(200).send(users);
 })
 
+// Login Check
+
 app.post('/login', (request, response) => {
-    console.log("Hello login");
+   // console.log("Hello login");
     //console.log(request.body.Username);
     var uname = request.body.Username;
     var upw = request.body.Password;
+    console.log("request body",request.body);
     var logUser = null;
     users.forEach((user) => {
         if (uname === user.userName && upw === user.password) {
@@ -80,6 +80,48 @@ app.post('/login', (request, response) => {
     }
 
 })
+
+// Creating new User
+
+app.post('/user/create',(request,response)=>{
+    console.log("Creating a new user")
+    var ID=request.body.id;
+
+    var user={
+        userId: request.body.id ,
+        firstName:request.body.fName ,
+        middleName:request.body.mName ,
+        lastName: request.body.lName,
+        userName: request.body.uName,
+        password: request.body.password,
+    }
+    users.push(user);
+    var news=ID;
+    console.log(users);
+   // console.log(news);
+
+    var respo={
+        acknowledgement:{
+            status:"",
+            message:""
+        },
+        data:null
+    };
+
+    if(news){
+        respo.acknowledgement.status="Success";
+        respo.acknowledgement.message="User Created Successful";
+        respo.data=user;
+        response.status(200).send(respo); 
+    }  else{
+        respo.acknowledgement.status="Fail";
+        respo.acknowledgement.message="Creation Failed";
+        respo.data=users.user;
+        response.status(200).send(respo); 
+    }
+})
+
+//
 
 app.post('/addition', (request, response) => {
     console.log("Addition taking place.")
