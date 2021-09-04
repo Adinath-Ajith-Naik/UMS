c28e6f74c38a1f53c13f29e6a6fed0c93ab48bf1
ADINATH AJITH NAIK <naik.adi2001@gmail.com>
2021-09-04 18:29:19 +0530

    Added Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '',
     children: [
-      { path: '', component: AppComponent },
+      { path: '', component: HomeComponent },
       { path: 'create', loadChildren: () => import('./pages/user-management-system/user-management-system.module').then((m) => m.UserManagementSystemModule)}
     ],
   },
