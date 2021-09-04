ca915ec6f5dd5b917400fdfaafd2898c9f0b108f
ADINATH AJITH NAIK <naik.adi2001@gmail.com>
2021-09-04 18:15:13 +0530

    Angular Routing Config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    children: [
+      { path: '', component: AppComponent },
+      { path: 'create', loadChildren: () => import('./pages/user-management-system/user-management-system.module').then((m) => m.UserManagementSystemModule)}
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/user-management-system/user-management-system.module.ts
+++ src/app/pages/user-management-system/user-management-system.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: CreateUserComponent }
+];
 
 @NgModule({
   declarations: [
     CreateUserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   exports:[
-    CreateUserComponent
+    CreateUserComponent,
   ]
 })
 export class UserManagementSystemModule { }
